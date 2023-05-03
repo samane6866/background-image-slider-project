7d912587b16a425a564d2ba3c781add6962249b8
samane6866 <samane.bahramian@gmail.com>
2023-05-03 13:37:49 +0200

    image slider done

--- js/app.js
+++ js/app.js
@@ -3,7 +3,27 @@ const pictures = [
   "contBcg-1",
   "contBcg-2",
   "contBcg-3",
-  "contBcg-4"
+  "contBcg-4",
 ];
 
 // buena suerte
+let indexPhoto = 1;
+
+let imageContainer = document.querySelector(".img-container");
+
+let buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log(button);
+    if (button.classList.contains("btn-right")) {
+      indexPhoto == pictures.length - 1 ? (indexPhoto = 0) : indexPhoto++;
+      imageContainer.style.backgroundImage = `url(../img/${pictures[indexPhoto]}.jpeg)`;
+    } else {
+      indexPhoto == 0 ? (indexPhoto = pictures.length - 1) : indexPhoto--;
+      imageContainer.style.backgroundImage = `url(../img/${pictures[indexPhoto]}.jpeg)`;
+
+      console.log("hola");
+    }
+  });
+});
